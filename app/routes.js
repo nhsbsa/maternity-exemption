31e458fbe1e5eb4748793bad2904b51e82b8658b
Kasturi Sithembaram <kasturisithembaram@NhsbsaMacbookadmins-MacBook-Pro-2.local>
2019-08-02 14:07:48 +0100

    commit

--- app/routes.js
+++ app/routes.js
@@ -6,6 +6,20 @@ router.get('/', function (req, res) {
   res.render('index')
 })
 
+
+
+router.get(/email-account-handler/, function (req, res) {
+  if (req.query.radiocontactgroup == 'Yes') {
+    res.redirect('../new-registration/nmc-gmc');
+  } else if (req.query.radiocontactgroup == 'No') {
+    res.redirect('https://support.nhs.net/knowledge-base/registering-for-an-nhsmail-account/');
+  }
+});
+
+
+
+
+
 // add your routes here
 
 module.exports = router
