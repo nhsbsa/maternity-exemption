58a13dc406d551746b252f59f17c3d1b047d9ec3
Kasturi <kasturi.sithembaram@nhs.net>
2019-11-29 12:01:41 +0000

    commit

--- app/routes.js
+++ app/routes.js
@@ -234,7 +234,7 @@ router.get(/nhsnum-handler/, function (req, res) {
 
 router.get(/nmcgmc-handler/, function (req, res) {
   if (req.query.nmcgmc == 'gmc') {
-    res.redirect('register-done');
+    res.redirect('reg-done');
   } else if (req.query.nmcgmc == 'nmc') {
     res.redirect('register-nmc');
   }
